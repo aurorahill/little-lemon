35050f3a677b85d190a7a1bf800435cd9fe54e84
Katarzyna Lubecka <katarzyna.lubecka93@gmail.com>
2024-10-14 10:00:18 +0200

    Simplify the boilerplate app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem,
+      cumque laboriosam aliquid accusantium in placeat, dignissimos nostrum
+      aspernatur autem obcaecati expedita quasi quia, sequi culpa aperiam.
+      Facilis eos dolores atque.
     </div>
   );
 }
